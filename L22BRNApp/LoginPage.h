2402b7a968876d3dc221045c7a5e1731c7a2e241
student <student@Achievers-iMac.local>
2017-06-12 16:29:15 +0530

    shivaji commit

---
+++ L22BRNApp/LoginPage.h
@@ -0,0 +1,13 @@
+//
+//  LoginPage.h
+//  L22BRNApp
+//
+//  Created by student on 6/12/17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginPage : UIViewController
+
+@end
---
+++ L22BRNApp/LoginPage.m
@@ -0,0 +1,40 @@
+//
+//  LoginPage.m
+//  L22BRNApp
+//
+//  Created by student on 6/12/17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import "LoginPage.h"
+
+@interface LoginPage ()
+
+@end
+
+@implementation LoginPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    NSLog(@"login page created by shvaji");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
