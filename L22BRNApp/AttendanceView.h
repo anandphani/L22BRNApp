05c4aa6a9e944541e3b134995320d078449a4ecc
student <student@Achievers-iMac.local>
2017-06-12 16:56:35 +0530

    gowtham

---
+++ L22BRNApp/AttendanceView.h
@@ -0,0 +1,13 @@
+//
+//  AttendanceView.h
+//  L22BRNApp
+//
+//  Created by student on 6/12/17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttendanceView : UIViewController
+
+@end
---
+++ L22BRNApp/AttendanceView.m
@@ -0,0 +1,39 @@
+//
+//  AttendanceView.m
+//  L22BRNApp
+//
+//  Created by student on 6/12/17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import "AttendanceView.h"
+
+@interface AttendanceView ()
+
+@end
+
+@implementation AttendanceView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"created by gowtgham");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
